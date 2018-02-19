0fa19103fbe6cfa16cffe72bbdf5ba58e4417070
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-02-19 13:12:51 +0000

    added main-nav, home, 404, services components. Added routing, added variable sass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './public/components/home/home.component';
+import { ServicesComponent } from './public/components/services/services.component';
+import { NotFoundComponent } from './public/components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './public/components/main-nav/main-nav.component';
+import { HomeComponent } from './public/components/home/home.component';
+import { ServicesComponent } from './public/components/services/services.component';
+import { NotFoundComponent } from './public/components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent,
+    ServicesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
