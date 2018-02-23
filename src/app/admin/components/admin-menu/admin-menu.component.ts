f93fcfadbfe7cffacd9c7034d544da3061fe173e
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-02-23 20:33:32 +0000

    Started admin area page

--- src/app/admin/components/admin-menu/admin-menu.component.ts
+++ src/app/admin/components/admin-menu/admin-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-menu',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-menu.component.scss']
 })
 export class AdminMenuComponent implements OnInit {
+  theUser: string;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.theUser = this.userService.loggedInUser;
   }
 
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
 }
