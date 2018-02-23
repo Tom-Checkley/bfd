65dd51994b6372ef09c4bedd00b938a3729ba80c
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-02-23 20:04:06 +0000

    Login component

--- src/app/admin/components/login/login.component.ts
+++ src/app/admin/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password1: string;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.userService.login(this.email, this.password1);
+    this.userService.verifyUser();
+  }
+
+  signUp() {
+    this.router.navigate(['/admin/register']);
   }
 
 }
