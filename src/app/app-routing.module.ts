1f7e05671fd346efd82aa5e71048b2450f872c19
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-02-28 07:07:27 +0000

    routing for public blog page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './public/components/home/home.component';
 import { ServicesComponent } from './public/components/services/services.component';
+import { BlogComponent } from './public/components/blog/blog.component';
 import { NotFoundComponent } from './public/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'services', component: ServicesComponent },
+  { path: 'blog', component: BlogComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
